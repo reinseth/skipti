19e1536874e6d01ebead06145a19f609d17d0a5a
Gunnar André Reinseth <gar@prepost.net>
2024-10-09 22:42:01 +0200

    Setup some state and action management
    Using the approach outlined by Christian and Magnar on several occasions:
    - https://www.kodemaker.no/blogg/2020-01-enkel-arkitektur/ (Norwegian)
    - https://2023.javazone.no/program/85f23370-440f-42b5-bf50-4cb811fef44d

--- src/skipti/main.cljs
+++ src/skipti/main.cljs
@@ -1,10 +1,51 @@
 (ns skipti.main
-  (:require [replicant.dom :as replicant]))
+  (:require [cljs.core.match :refer-macros [match]]
+            [clojure.walk :as walk]
+            [replicant.dom :as replicant]))
 
-(defn render []
-  (replicant/render
-   (js/document.getElementById "root")
-   [:div "Skipti!"]))
+(defonce store (atom {}))
+
+(defn interpolate-dom-values [dom-event action]
+  (walk/postwalk
+   (fn [x]
+     (case x
+       :dom-event.target/value
+       (some-> dom-event .-target .-value)
+
+       x))
+   action))
+
+(defn handle-action [state action]
+  (apply js/console.log action)
+  (match action
+    [:assoc-in ks v]
+    (assoc-in state ks v)))
+
+(defn handle-actions [actions]
+  (swap! store (fn [state]
+                 (->> actions
+                      (filter identity)
+                      (reduce handle-action state)))))
+
+(defn handle-dom-event [{:replicant/keys [^js js-event]} actions]
+  (.preventDefault js-event)
+  (.stopPropagation js-event)
+  (when actions
+    (handle-actions (map #(interpolate-dom-values js-event %) actions))))
+
+(defn render
+  ([]
+   (render @store))
+  ([state]
+   (replicant/render
+    (js/document.getElementById "root")
+    [:div "Skipti!" (pr-str state)])))
 
 (defn ^:export init []
+  (add-watch store ::me (fn [_ _ _ state]
+                          (render state)))
+  (replicant/set-dispatch! handle-dom-event)
   (render))
+
+(comment
+  (handle-actions [[:assoc-in [:foo] "Bar"]]))
