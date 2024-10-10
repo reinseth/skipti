adebd2541fa091f760456ff7f2ee24ae8499b124
Gunnar André Reinseth <gar@prepost.net>
2024-10-10 08:26:49 +0200

    Setup tests

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ((nil
   . ((cider-preferred-build-tool . shadow-cljs)
      (cider-default-cljs-repl . shadow)
-     (cider-shadow-watched-builds . ("app"))
+     (cider-shadow-watched-builds . ("app" "test"))
      (cider-offer-to-open-cljs-app-in-browser . nil))))
---
+++ test/skipti/squad_view_test.cljs
@@ -0,0 +1,19 @@
+(ns skipti.squad-view-test
+  (:require [skipti.squad-view :as sut]
+            [cljs.test :refer-macros [deftest is testing]]))
+
+(deftest prep-squad-view-test
+  (testing "add player action"
+    (is (= (:add-player (sut/prep-squad-view {:squad/input "Margaret"}))
+           [[:assoc-in [:squad/players] #{"Margaret"}]
+            [:assoc-in [:squad/input] ""]]))
+    (is (= (:add-player (sut/prep-squad-view {:squad/players #{"Jenny" "Margaret"}
+                                              :squad/input "Margaret"}))
+           [[:assoc-in [:squad/players] #{"Jenny" "Margaret"}]
+            [:assoc-in [:squad/input] ""]])))
+
+  (testing "add player action for empty input"
+    (is (= (:add-player (sut/prep-squad-view {}))
+           []))
+    (is (= (:add-player (sut/prep-squad-view {:squad/input " "}))
+           []))))
