8cb922b134cd3bb24b8f3a84d15c4495e09e9545
Gunnar André Reinseth <gar@prepost.net>
2024-10-15 23:14:51 +0200

    Make release

--- dist.sh
+++ dist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf dist
-mkdir -p dist/js
+rm -rf docs
+mkdir -p docs/js
 npx shadow-cljs release app
-cp build/js/main.js dist/js
-cp -R public/* dist
+cp build/js/main.js docs/js
+cp -R public/* docs
