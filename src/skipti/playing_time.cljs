b06f50238ee7c50f10142d9557a4561209eb2224
Gunnar André Reinseth <gar@prepost.net>
2024-10-15 21:54:59 +0200

    Add functions for calculating playing time

---
+++ src/skipti/playing_time.cljs
@@ -0,0 +1,95 @@
+(ns skipti.playing-time
+  (:require [cljs.core.match :refer-macros [match]]
+            [clojure.math :as math]))
+
+(defn ->intervals [events entity from to]
+  (reduce
+   (fn [acc [e v t]]
+     (if-not (= e entity)
+       acc
+       (cond
+         (= v from)
+         (conj acc [t])
+
+         (= v to)
+         (let [i (last acc)]
+           (if i
+             (conj (pop acc) (conj i t))
+             (conj acc [nil t])))
+
+         :else
+         acc)))
+   []
+   events))
+
+(defn interval-start [x]
+  (or (first x) js/Number.MIN_SAFE_INTEGER))
+
+(defn interval-end [x]
+  (or (second x) js/Number.MAX_SAFE_INTEGER))
+
+(defn interval-intersects-start [x y]
+  (and (<= (interval-start x) (interval-start y))
+       (> (interval-end x) (interval-start y))))
+
+(defn interval-intersects-end [x y]
+  (and (< (interval-start x) (interval-end y))
+       (>= (interval-end x) (interval-end y))))
+
+(defn interval-contains [x y]
+  (and (<= (interval-start x) (interval-start y))
+       (>= (interval-end x) (interval-end y))))
+
+(defn interval-intersection [x y]
+  (cond
+    (interval-contains x y)
+    y
+
+    (interval-contains y x)
+    x
+
+    (interval-intersects-start y x)
+    [(interval-start x) (interval-end y)]
+
+    (interval-intersects-end y x)
+    [(interval-start y) (interval-end x)]
+
+    :else
+    nil))
+
+;; TODO send in periods og player-intervals
+(defn player-time [events cur-time player]
+  (let [periods (->intervals events :match :started :stopped)
+        player-intervals (->intervals events player :on :off)]
+    (max (->> player-intervals
+              (mapcat (fn [i]
+                        (filter identity (map (fn [p] (interval-intersection i p)) periods))))
+              (reduce (fn [acc i]
+                        (match i
+                          [start end]
+                          (+ acc (- end start))
+
+                          [start]
+                          (+ acc (- cur-time start))
+
+                          :else
+                          acc))
+                      0))
+         0)))
+
+(defn mins [ms]
+  (math/floor (/ ms (* 1000 60))))
+
+(defn secs [ms]
+  (math/floor (/ (rem (rem ms 3600000) 60000) 1000)))
+
+(defn format-time [ms]
+  (let [ms (max ms 0)
+        m (mins ms)
+        s (secs ms)]
+    (str (if (< m 10) (str "0" m) m)
+         ":"
+         (if (< s 10) (str "0" s) s))))
+
+(defn players-with-time [events cur-time players]
+  (map (juxt identity #(mins (player-time events cur-time %))) players))
---
+++ test/skipti/playing_time_test.cljs
@@ -0,0 +1,117 @@
+(ns skipti.playing-time-test
+  (:require [cljs.test :refer-macros [deftest is]]
+            [skipti.playing-time :as sut]))
+
+(deftest ->intervals-test
+  (is (= (sut/->intervals
+          [["Janne" :on 0]
+           ["Janne" :off 1]
+           ["Janne" :on 2]
+           ["Janne" :off 3]
+           [:match :started 1]]
+          :match
+          :stopped
+          :started)
+         [[nil 1]]))
+
+  (is (= (sut/->intervals
+          [["Janne" :on 0]
+           ["Janne" :off 1]
+           ["Janne" :on 2]
+           ["Janne" :off 3]
+           [:match :started 3]]
+          "Janne"
+          :on
+          :off)
+         [[0 1] [2 3]]))
+
+  (is (= (sut/->intervals
+          [["Janne" :on 0]
+           ["Janne" :off 1]
+           [:match :started 1]
+           ["Janne" :on 2]
+           ["Janne" :off 3]
+           ["Janne" :on 2]
+           [:match :stopped 9]]
+          "Janne"
+          :on
+          :off)
+         [[0 1] [2 3] [2]]))
+
+  (is (= (sut/->intervals
+          [["Janne" :on 0]
+           ["Janne" :off 1]
+           [:match :started 1]
+           ["Janne" :on 2]
+           ["Janne" :off 3]
+           ["Janne" :on 2]
+           [:match :stopped 9]]
+          :match
+          :stopped
+          :started)
+         [[nil 1] [9]])))
+
+(deftest interval-intersection-start-test
+  (is (false? (sut/interval-intersects-start [0 5] [5 15])))
+  (is (true? (sut/interval-intersects-start [0 10] [5 15])))
+  (is (true? (sut/interval-intersects-start [5 16] [5 15])))
+  (is (true? (sut/interval-intersects-start [nil 15] [10 20])))
+  (is (false? (sut/interval-intersects-start [11 21] [10 20]))))
+
+(deftest interval-intersects-end-test
+  (is (true? (sut/interval-intersects-end [5 15] [4 14])))
+  (is (true? (sut/interval-intersects-end [3 15] [4 14])))
+  (is (false? (sut/interval-intersects-end [4 13] [4 14])))
+  (is (false? (sut/interval-intersects-end [14 15] [4 14]))))
+
+(deftest interval-intersection-test
+  (is (= (sut/interval-intersection [0 10] [5 15])
+         [5 10]))
+  (is (= (sut/interval-intersection [0 10] [-5 5])
+         [0 5]))
+  (is (= (sut/interval-intersection [0 10] [1 9])
+         [1 9]))
+  (is (= (sut/interval-intersection [0 nil] [1 nil])
+         [1 nil])))
+
+(deftest player-time-test
+  (is (= (sut/player-time
+          [["Janne" :on 0]
+           [:match :started 1]
+           ["Janne" :off 3]
+           ["Janne" :on 4]
+           [:match :stopped 9]]
+          10
+          "Janne")
+         7))
+
+  (is (= (sut/player-time
+          [["Janne" :on 0]
+           [:match :started 1]
+           ["Janne" :off 3]
+           ["Janne" :on 4]]
+          10
+          "Janne")
+         8))
+
+  (is (= (sut/player-time
+          [["Janne" :on 0]
+           [:match :started 1]
+           ["Janne" :off 3]
+           ["Janne" :on 4]
+           [:match :stopped 5]
+           [:match :started 6]
+           ["Janne" :off 7]]
+          10
+          "Janne")
+         4)))
+
+(deftest format-time-test
+  (is (= (sut/format-time 0)
+         "00:00"))
+  (is (= (sut/format-time 12321)
+         "00:12"))
+  (is (= (sut/format-time 105500)
+         "01:45"))
+  (is (= (sut/format-time (* 61 60000))
+         "61:00")))
